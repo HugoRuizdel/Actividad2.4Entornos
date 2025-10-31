7faaeba00437260a5ee77ec4cc0a7da51ad6bbca
hugo <hruicas481@g.educaand.es>
2025-10-31 09:47:57 +0100

    PrimerosPasosPrimerCambio

--- src/Holamundo.java
+++ src/Holamundo.java
@@ -1,5 +1,11 @@
+import java.util.Scanner;
+
 public class Holamundo {
     public static void main(String[] args) throws Exception {
         System.out.println("Hello, World!");
+        Scanner sc = new Scanner(System.in);
+        int n=1,m=2,resultado;
+        resultado=n*m;
+        System.out.println(resultado);
     }
 }
